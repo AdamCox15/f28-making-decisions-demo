706d10a38bbc4c1d8d7f381a6de3b40197fc416b
Adam Cox <adamcox.dev@gmail.com>
2022-08-18 12:40:15 -0600

    First part of the arrays demo

--- arrays.js
+++ arrays.js
@@ -1,4 +1,16 @@
-/*
-    In this file, we'll be continuing our story
-    with Jon being allowed to flee his fight with Jamie.
-*/
+let backpack = [];
+
+backpack.push('Jedi Robe');
+backpack.push('Lightsaber');
+backpack.push('Crippling sense of failure and guilt');
+
+backpack.splice(1,1);
+backpack.push('Wookie Coat');
+
+backpack.pop();
+backpack.push('Blue Milk', 'Baby Yoda', 'a child named Luke', 'Com Device', 'High Ground', 'Blaster');
+
+let stroller = backpack.splice(3,2);
+
+console.log(backpack);
+console.log(stroller);
